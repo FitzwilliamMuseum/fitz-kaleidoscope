c2664d055a973c5edf99c6868f97f3f2c8d38e59
Oliver Warren <66948369+o-s-warren@users.noreply.github.com>
2021-03-12 12:51:31 +0000

    Update javascript to show and hide logo

--- js/buttons.js
+++ js/buttons.js
@@ -1,10 +1,12 @@
 const infoBox = document.getElementById("infoBox")
+const logo = document.getElementById("logo")
 
 function hideInfo () {
   infoBox.style.display = "none";
-  
+  logo.style.display = "none";
 }   
 
 function showInfo () {
   infoBox.style.display = "flex";
-}          
+  logo.style.display = "flex";
+}                     
